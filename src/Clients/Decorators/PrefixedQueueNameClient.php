2599aea1372dcbc265f3c55511d026d7d77dbe00
Julian Prud'homme <jprudhomme@naoned.fr>
2017-03-15 12:32:14 +0100

    feat: add a decorator to prefix queue name

---
+++ src/Clients/Decorators/PrefixedQueueNameClient.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Puzzle\AMQP\Clients\Decorators;
+
+use Puzzle\AMQP\Client;
+use Puzzle\AMQP\WritableMessage;
+use Psr\Log\LoggerAwareTrait;
+
+class PrefixedQueueNameClient implements Client
+{
+    const
+        DELIMITER = '.';
+
+    use LoggerAwareTrait;
+
+    private
+        $client,
+        $queueNamePrefix;
+
+    public function __construct(Client $client, $queueNamePrefix)
+    {
+        $this->client = $client;
+        $this->queueNamePrefix = $queueNamePrefix;
+    }
+
+    public function publish($exchangeName, WritableMessage $message)
+    {
+        return $this->client->publish($exchangeName, $message);
+    }
+
+    public function getQueue($queueName)
+    {
+        $prefixedQueueName = $this->computePrefixedQueueName($queueName);
+
+        return $this->client->getQueue($prefixedQueueName);
+    }
+
+    public function getExchange($exchangeName)
+    {
+        return $this->client->getExchange($exchangeName);
+    }
+
+    private function computePrefixedQueueName($queueName)
+    {
+        $queueNameParts = [];
+
+        if(! empty($this->queueNamePrefix))
+        {
+            $queueNameParts[] = trim($this->queueNamePrefix);
+        }
+
+        $queueNameParts[] = $queueName;
+
+        return trim(implode(self::DELIMITER, $queueNameParts));
+    }
+}
