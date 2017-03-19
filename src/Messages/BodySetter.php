3e24b825373294a8cc2e059b69fa508fd0404783
Niktux <nico.ln@gmail.com>
2017-03-19 02:14:10 +0100

    feat(message): fludi interfaces on body helpers

--- src/Messages/BodySetter.php
+++ src/Messages/BodySetter.php
@@ -11,16 +11,22 @@ trait BodySetter
     public function setText($text)
     {
         $this->setBody(new Text($text));
+
+        return $this;
     }
 
     public function setJson(array $content)
     {
         $this->setBody(new Json($content));
+
+        return $this;
     }
 
     public function setBinary($content)
     {
         $this->setBody(new Binary($content));
+
+        return $this;
     }
 
     abstract public function setBody(Body $body);
