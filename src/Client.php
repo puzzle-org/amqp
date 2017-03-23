6485d802653b44489cffe487f570713f6f488195
Niktux <nico.ln@gmail.com>
2017-03-23 23:56:04 +0100

    refactor(workers): remove old public property (queue) and replace direct accesses by getter calls
    closes #21

--- src/Client.php
+++ src/Client.php
@@ -6,9 +6,18 @@ use Psr\Log\LoggerAwareInterface;
 
 interface Client extends LoggerAwareInterface
 {
+    /**
+     * @return boolean
+     */
     public function publish($exchangeName, WritableMessage $message);
 
+    /**
+     * @return \AMQPQueue
+     */
     public function getQueue($queueName);
 
+    /**
+     * @return \AMQPExchange
+     */
     public function getExchange($exchangeName);
 }
