6abc1f29eae562a7f5f2a45862215b42e087667e
Claude LE BRIS <claude.lebris@gmail.com>
2024-12-18 12:02:34 +0100

    chore(composer): run composer from container

--- makefiles/composer.mk
+++ makefiles/composer.mk
@@ -1,28 +1,90 @@
 #------------------------------------------------------------------------------
 # Composer
 #------------------------------------------------------------------------------
+
+COMPOSER_VERSION?=2
+
+#------------------------------------------------------------------------------
+
+#
+# COMPOSER_OPTION
+#    This variable defines options always passed to composer.
+#
+#    Variables explanations :
+#        --no-plugins : do not execute nor prompt for plugin authorization at deps installation/update.
+#                       Avoid composer updating the "allow-plugins" list in composer.json by itself
+#
+COMPOSER_OPTIONS=--no-plugins
+
+#------------------------------------------------------------------------------
+
+composer = $(DOCKER_RUN) --rm \
+                -v ${HOST_SOURCE_PATH}:/var/www/app \
+                -v ~/.cache/composer:/tmp/composer \
+                -e COMPOSER_CACHE_DIR=/tmp/composer \
+                -w /var/www/app \
+                -u ${USER_ID}:${GROUP_ID} \
+                composer:${COMPOSER_VERSION} ${COMPOSER_OPTIONS} $(COMPOSER_INTERACTIVE) $1 $2
+
+# Spread cli arguments
+ifneq (,$(filter $(firstword $(MAKECMDGOALS)),composer))
+    CLI_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+    $(eval $(CLI_ARGS):;@:)
+endif
+
+# Add ignore platform reqs for composer install & update
 COMPOSER_ARGS=
 ifeq (composer, $(firstword $(MAKECMDGOALS)))
-    ifneq (,$(filter install update,$(CLI_ARGS)))
+    ifneq (,$(filter install update require,$(CLI_ARGS)))
         COMPOSER_ARGS=--ignore-platform-reqs
     endif
 endif
 
-composer: composer.phar
-	php composer.phar $(CLI_ARGS) $(COMPOSER_ARGS)
+#------------------------------------------------------------------------------
 
-composer-install: composer.phar ## Install dependencies
-	php composer.phar install --ignore-platform-reqs
+.PHONY: composer
+composer: -composer-init ## Run composer
+	$(call composer, $(CLI_ARGS), $(COMPOSER_ARGS))
 
-composer-update: composer.phar
-	php composer.phar update --ignore-platform-reqs
+.PHONY: composer-install
+composer-install: -composer-init vendor/ ## Install dependencies via composer
 
-composer.phar:
-	curl -sS https://getcomposer.org/installer | php
+# If you want to edit (and commit) a library directly in vendor/
+# just rm -rf vendor/<lib_path> + make composer-install-sources
+.PHONY: composer-install-sources
+composer-install-sources: -composer-init
+	$(call composer, install, --ignore-platform-reqs --prefer-source)
 
-clean:
-	rm -f composer.lock
-	rm -f composer.phar
-	rm -rf vendor
+.PHONY: composer-update
+composer-update: -composer-init
+	$(call composer, update, --ignore-platform-reqs)
 
-.PHONY: composer composer-install composer-update clean
+.PHONY: composer-dumpautoload
+composer-dumpautoload: -composer-init
+	$(call composer, dumpautoload)
+
+.PHONY: composer-version
+composer-version: -composer-init
+	$(call composer, --version)
+
+#------------------------------------------------------------------------------
+# Non PHONY targets
+#------------------------------------------------------------------------------
+
+vendor/: composer.json
+	@$(call composer, install, --ignore-platform-reqs)
+
+#------------------------------------------------------------------------------
+
+.PHONY: -composer-init
+-composer-init: ~/.cache/composer
+
+~/.cache/composer:
+	mkdir -p ~/.cache/composer
+
+#------------------------------------------------------------------------------
+
+.PHONY: clean-composer
+clean-composer:
+	-rm -f composer.lock
+	-rm -rf vendor
