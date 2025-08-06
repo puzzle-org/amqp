7508c2b7b1d7e83ab5a3fd7e2eec8c671b3e5915
Claude LE BRIS <claude.lebris@gmail.com>
2025-08-06 12:40:16 +0200

    chore(karma): udpate karma and run in container

--- makefiles/karma.mk
+++ makefiles/karma.mk
@@ -2,10 +2,16 @@
 # Karma
 #------------------------------------------------------------------------------
 config: karma ## Run karma for behat tests
-	./karma hydrate -e behat
-
+	@$(DOCKER_RUN) --rm --name karma \
+               -v ${HOST_SOURCE_PATH}:${CONTAINER_SOURCE_PATH} \
+               -w ${CONTAINER_SOURCE_PATH} \
+               -u ${USER_ID}:${GROUP_ID} \
+               php:8.3-cli \
+               ./karma hydrate -e behat
 karma:
-	wget -O karma -q https://github.com/Niktux/karma/releases/download/5.6.0/karma.phar
+	$(eval LATEST_VERSION := $(shell curl -L -s -H 'Accept: application/json' https://github.com/niktux/karma/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/'))
+	@echo "Latest version of Karma is ${LATEST_VERSION}"
+	wget -O karma -q https://github.com/Niktux/karma/releases/download/${LATEST_VERSION}/karma.phar
 	chmod 0755 karma
 
 clean-karma:
